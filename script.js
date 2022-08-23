dedd6e88f205584c4bd824201652d228c7b95eca
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-08-23 10:24:48 -0400

    built HTML to get ready for addition of js

---
+++ script.js
@@ -0,0 +1,13 @@
+// TODO: Function: Set timer to end quiz when time === 0
+
+
+// TODO: Function: When answer is given, check if correct, if true, give point. Go to next question
+
+// TODO: After last question is answered. Show score and ask user to enter Initials
+
+// TODO: When user inputs initials, save initials and score to highscore leaderboards
+
+// TODO: Ask user if they want to take quiz again
+
+// Will need a for loop to display questions
+// Update a value for the questions
