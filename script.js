1cbce41a52bf455f35b30babb46321bdf02a6da7
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-08-23 23:27:12 -0400

    added css file and completed css for home page. changed highscore section to end. added new highscore section.

--- script.js
+++ script.js
@@ -12,72 +12,109 @@
 
 var homeEl = document.querySelector('#home');
 var quizEl = document.querySelector('#quiz');
+var endEl = document.querySelector('#end');
 var highscoreEl = document.querySelector('#highscore');
 var questionEl = document.querySelector('#questions');
+var answersEl = document.querySelector('#answers');
 var timerEl = document.querySelector('#timer');
 var timeRemaining = 60;
-
-// questions for quiz
-var questions = [
-    {   question: 'How is a file determined to be a JavaScript file?',
-        choices: [
-            '.Java',
-            '.JavaScript',
-            '.js',
-            '.JScript'
-        ],
-        right: choices[2]
-    },
-    {   question: 'How do you link a JavaScript file to HTML?',
-        choices: [
-            '<script src="./script.js></script>"',
-            '<script href="./script.js></script>"',
-            '<link src="./script.js>"',
-            '<link href="./script.js>"'
-        ],
-        right: choices[0]
-    },
-    {   question: 'How would you call a function named myFunction in JavaScript?',
-    choices: [
-        'call.myFunction();',
-        'myFunction();',
-        'myFunction()',
-        'call.MyFunction();'
-    ],
-    right: choices[1]
-    },
-    {   question: 'How do you define a variable in JavaScript?',
-    choices: [
-        'variable x = 5',
-        'x = 5',
-        'var x = 5;',
-        'var x = 5'
-    ],
-    right: choices[2]
-    },
-];
+var startButton = document.querySelector('#start');
 
 
-var 
+var init = function() {
+    setState('home');
+};
+
+// var nextQuestion = function() {
+
+// };
+
+// var correct = function() {
+
+// }
+
+// var wrong = function() {
+
+// };
+
+// var quizEnded = function() {
+//     setState('end')
+// };
+
+
+// // questions for quiz
+// var questions = [
+//     {   question: 'How is a file determined to be a JavaScript file?',
+//         choices: [
+//             '.Java',
+//             '.JavaScript',
+//             '.js',
+//             '.JScript'
+//         ],
+//         right: choices[2]
+//     },
+//     {   question: 'How do you link a JavaScript file to HTML?',
+//         choices: [
+//             '<script src="./script.js></script>"',
+//             '<script href="./script.js></script>"',
+//             '<link src="./script.js>"',
+//             '<link href="./script.js>"'
+//         ],
+//         right: choices[0]
+//     },
+//     {   question: 'How would you call a function named myFunction in JavaScript?',
+//     choices: [
+//         'call.myFunction();',
+//         'myFunction();',
+//         'myFunction()',
+//         'call.MyFunction();'
+//     ],
+//     right: choices[1]
+//     },
+//     {   question: 'How do you define a variable in JavaScript?',
+//     choices: [
+//         'variable x = 5',
+//         'x = 5',
+//         'var x = 5;',
+//         'var x = 5'
+//     ],
+//     right: choices[2]
+//     }
+// ];
+
+// var correct = 
+// var wrong = 
+
+startButton.addEventListener('click', function() {
+    setState('quiz');
+});
 
 // need to add css to set style of page when each shows
 var setState = function(state) {
     if (state === 'home') {
-        homeEl.setAttribute('style',);
-        quizEl.setAttribute('style', 'display: none');
-        highscoreEl.setAttribute('style', 'display: none');
+        homeEl.setAttribute('style', 'display: flex; flex-direction: column; background: aquamarine; font-size: 50px; text-align: center;');
+        quizEl.setAttribute('style', 'display: none;');
+        endEl.setAttribute('style', 'display: none;');
+        highscoreEl.setAttribute('style', 'display: none;');
     }
-    else if (state === 'quiz') {
-        homeEl.setAttribute('style', 'display: none');
-        quizEl.setAttribute('style',);
-        highscoreEl.setAttribute('style', 'display: none');
+    if (state === 'quiz') {
+        homeEl.setAttribute('style', 'display: none;');
+        quizEl.setAttribute('style', 'display: flex; flex-direction: column; background: yellow;');
+        endEl.setAttribute('style', 'display: none;');
+        highscoreEl.setAttribute('style', 'display: none;');
     }
-    else if (state === 'highscore') {
-        homeEl.setAttribute('style', 'display: none');
-        quizEl.setAttribute('style', 'display: none');
-        highscoreEl.setAttribute('style',);
+    if (state === 'end') {
+        homeEl.setAttribute('style', 'display: none;');
+        quizEl.setAttribute('style', 'display: none;');
+        endEl.setAttribute('style', 'display: flex; flex-direction:column; background: yellow;');
+        highscoreEl.setAttribute('style', 'display: none;');
     }
-
+    if (state === 'highscore') {
+        homeEl.setAttribute('style', 'display: none;');
+        quizEl.setAttribute('style', 'display: none;');
+        endEl.setAttribute('style', 'display: none;');
+        highscoreEl.setAttribute('style', 'display: flex; flex-direction: column; background: yellow;');
+    }
 };
 
-setState('home');
+init();
